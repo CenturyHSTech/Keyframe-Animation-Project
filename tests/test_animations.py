98ec122b7803d66f7aaba84bf1db111171a7d30c
hundredvisionsguy <winikkc@hsd.k12.or.us>
2025-01-21 17:42:32 -0800

    adding tests on keyframes and animation properties

--- tests/test_animations.py
+++ tests/test_animations.py
@@ -17,6 +17,18 @@ def test_for_number_of_animations():
     assert len(animation_report) >= 1
 
 
-keyframe_data = animation_tools.get_keyframe_report(project_dir, 6, 4, 0)
-for item in keyframe_data:
-    print(item)
+keyframe_data = animation_tools.get_keyframe_report(project_dir, 6, 4)
+
+
+@pytest.mark.parametrize("results", keyframe_data)
+def test_for_4_percent_or_6_overall_keyframes(results):
+    assert "pass:" in results
+
+
+animation_properties_data = animation_tools.get_animation_properties_report(
+    project_dir, 4)
+
+
+@pytest.mark.parametrize("results", animation_properties_data)
+def test_for_four_unique_animation_properties(results):
+    assert "pass:" in results
